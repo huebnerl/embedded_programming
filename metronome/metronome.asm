03e29f0ca1f203bdbab4f3fbfa9210a2ac9648b5
huebnerl <LarsHuebner2@web.de>
2018-06-06 15:34:58 +0200

    Added comments.

--- metronome/metronome.asm
+++ metronome/metronome.asm
@@ -47,43 +47,43 @@ mov th1, timerValue
 ; Recording section
 ; -----------------
 loop:
-mov P0, #0fh
+mov P0, #0fh				; set the first 4 bits to 1 (for the key matrix column checking) Port0
 mov r7, P0
-cjne r7, #0Eh, notpressed
+cjne r7, #0Eh, notpressed	; check if the corresponding column is unset (key matrix is invers!)
 
-mov P0, #0f0h
+mov P0, #0f0h				; set the last 4 bits to 1 (for row checking)
 mov r7, P0
-cjne r7, #70h, notpressed
-cjne r6, #00h, loop
+cjne r7, #70h, notpressed	; check the row that is set
+cjne r6, #00h, loop			; check if timer running is not set -> continie and start recording-timer
 call startrectimer
 jmp loop
 
 notpressed:
-cjne r6, #01h, loop
+cjne r6, #01h, loop			; check if timer running is set -> stop recording-timer
 jmp stoprectimer
 
 startrectimer:
 mov tl0, th0
-setb tr0 ; start timer0
-mov r6, #01h
+setb tr0 					; start timer0
+mov r6, #01h				; set running flag
 ret
 
 stoprectimer:
-clr tr0 ; stop timer0
-mov r6, #00h
-cjne r4, #02h, incrementDivider
-inc r4
+clr tr0 					; stop timer0
+mov r6, #00h				; unset running flag
+cjne r4, #02h, incrementDivider		; count number of mesurements  (want 3 mesurements -> 3-1 = 2
+inc r4						; 2+1 = 3
 mov A, r5
 mov B, r4
-div AB
-mov maxTickCount, A
-jmp displaySection
+div AB						; divide mesurements-sum through number of mesurements
+mov maxTickCount, A			; bring result to register
+jmp displaySection			; start metronom display
 
 incrementDivider:
 inc r4
 jmp loop
 
-rectimer:
+rectimer:					; handle Interrupt
 inc r5
 ret
 
